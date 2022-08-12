59787e5be9de1d9152039a329e717f40ad852715
0legPr <107127388+0legPr@users.noreply.github.com>
2022-08-12 10:32:11 +0300

    Add files via upload

---
+++ Startico/script/sctipt.js
@@ -0,0 +1,25 @@
+let body = document.querySelector('body');
+// burger
+let nav = document.querySelector('.header__nav');
+let burger = document.querySelector('.header__burger');
+
+function mob_nav() {
+	nav.classList.toggle('active');
+	burger.classList.toggle('active');
+  body.classList.toggle('burger');
+}
+burger.onclick =  mob_nav;
+
+// alert 
+let alert_div = document.querySelector('.alert');
+let header__wrapper = document.querySelector('.header__wrapper');
+let alert_close = document.querySelector('.alert-close');
+
+function alert_fun() {
+	alert_div.classList.toggle('active');
+	header__wrapper.classList.toggle('alert-active');
+}
+
+alert_close.onclick = alert_fun;
+
+setTimeout(alert_fun, 3000);
